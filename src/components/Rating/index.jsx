40f5190f644c5774a70cc15ad2fa11bb40f84d6d
Jan Sleichert <jan.sleichert@gmail.com>
2024-10-18 16:48:33 +0200

    Splneni zadani

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -4,16 +4,22 @@ import './style.css';
 
 export const Rating = () => {
   const [rating, setRating] = useState(0);
+
+
   
+const rate = (value) => {
+  setRating(value)
+}
+
   return (
     <div className="rating">
       <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star value={1} onSelect={rate} glowing={rating >=1}/>
+        <Star  value={2} onSelect={rate} glowing={rating >=2}/>
+        <Star value={3} onSelect={rate} glowing={rating >=3}/>
+        <Star value={4} onSelect={rate} glowing={rating >=4}/>
+        <Star value={5} onSelect={rate} glowing={rating >=5}/>
       </div>
     </div>
   );
